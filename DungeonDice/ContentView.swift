86c8c618060f23f9619b09d5c52135f4014c86a7
Donglinnn <65762101+Donglinnn@users.noreply.github.com>
2024-07-23 10:15:53 +0800

    Initial Commit

---
+++ DungeonDice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DungeonDice
+//
+//  Created by Che-lun Hu on 2024/7/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DungeonDice/DungeonDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DungeonDiceApp.swift
+//  DungeonDice
+//
+//  Created by Che-lun Hu on 2024/7/23.
+//
+
+import SwiftUI
+
+@main
+struct DungeonDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
