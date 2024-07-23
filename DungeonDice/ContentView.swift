e1a6707d4a367f2a358af8d96d3c74559f9192f0
Donglinnn <65762101+Donglinnn@users.noreply.github.com>
2024-07-23 11:30:55 +0800

    YouTube Video ch 4 Start. End of ch. 4.3. Learning enums, Foreach.

--- DungeonDice/ContentView.swift
+++ DungeonDice/ContentView.swift
@@ -8,12 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    enum Dice: Int, CaseIterable {
+        case four = 4, six = 6, eight = 8, ten = 10, twelve = 12, twenty = 20, hundred = 100
+        
+        func roll() -> Int {
+            return Int.random(in: 1...self.rawValue)
+        }
+    }
+    
+    @State private var resultMessage = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Dungeon Dice")
+                .font(.largeTitle)
+                .fontWeight(.black)
+                .foregroundStyle(Color.red)
+            
+            Spacer()
+            
+            Text(resultMessage)
+                .font(.largeTitle)
+                .fontWeight(.medium)
+                .frame(height: 150)
+                .multilineTextAlignment(.center)
+            
+            Spacer()
+            
+            Group {
+                ForEach(Dice.allCases, id: \.self) { dice in
+                    Button("\(dice.rawValue)-sided") {
+                        resultMessage = "You rolled a \(Dice.four.roll()) on a \(Dice.four.rawValue)-sided dice."
+                    }
+                }
+            }
+            .buttonStyle(.borderedProminent)
+            .tint(.red)
+            
         }
         .padding()
     }
